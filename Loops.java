a2c3114fb975a5ed9e47b8b72aa6c186855b3357
ykshushantsingh-stack <ykshushantsingh@gmail.com>
2025-09-16 13:08:10 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+     for (int i =1; i <=n; i++) {
+        System.out.println(i *2);
+}
+}
+}
